55462ad21488ee8f603d3a15046e48b316c50780
mvaltierra <mvaltierra@birt.eus>
2025-09-15 01:26:32 +0200

    taprendizaje01 - Mi primer programa

---
+++ PRO/Unit00/src/org/taprendizaje01/HolaMundo.java
@@ -0,0 +1,25 @@
+/*********************************************************************************************************
+ * 
+ * Nombre: mvaltierra
+ * Fecha: 12/09/2025
+ * Modulo: Programación. 
+ * UD00. 
+ * Tarea: PROG00 - Tarea Aprendizaje 01: Primer programa
+ * Descripción del programa: Primer programa
+ * Autoevaluación: (dirección fichero autoevaluación)
+ * 
+ *********************************************************************************************************/
+
+
+package org.taprendizaje01; 
+
+public class HolaMundo {
+	
+	// Método Main desde donde se lanza el programa para su ejecución
+	public static void main(String[] args) {
+		
+		// Línea para la visualización del texto indicado
+		System.out.println("Hola Mundo");
+	}
+
+}
