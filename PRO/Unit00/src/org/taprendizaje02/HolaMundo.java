37a366bce899ed60859a44b22957ff9029d0eefe
mvaltierra <mvaltierra@birt.eus>
2025-09-15 01:49:05 +0200

    taprendizaje02 - Estructura de un programa

---
+++ PRO/Unit00/src/org/taprendizaje02/HolaMundo.java
@@ -0,0 +1,23 @@
+/*************************************************************************************
+ * 
+ * Nombre: mvaltierra
+ * Fecha: 12/09/2025
+ * Modulo: Programación. 
+ * UD00. 
+ * Tarea: PROG00 - Tarea Aprendizaje 02: Estructura Básica programa
+ * Descripción del programa: Estructura Básica programa
+ * Autoevaluación: (dirección fichero autoevaluación)
+ * 
+ ************************************************************************************/
+
+
+package org.taprendizaje02;
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println();
+		System.out.println("¿Qué tal?");
+		System.out.println("Hola \"amigo\"");	
+	}	
+}
