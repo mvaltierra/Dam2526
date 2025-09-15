e4a66e3888dd44437c9d212b0695cd8889c32aac
mvaltierra <mvaltierra@birt.eus>
2025-09-15 02:38:35 +0200

    tpractica01 - Corregir y ejecutar

---
+++ PRO/Unit00/src/org/tpractica01/HolaMundoAvanzado.java
@@ -0,0 +1,54 @@
+/*************************************************************************************
+ * 
+ * Nombre: mvaltierra
+ * Fecha: 12/09/2025
+ * Modulo: Programación. 
+ * UD00. 
+ * Tarea: PROG00 - Tarea Practica 01: Corregir y ejecutar un programa
+ * Descripción del programa: Corregir y ejecutar programa
+ * Autoevaluación: (dirección fichero autoevaluación)
+ * 
+ ************************************************************************************/
+
+/*
+ * Programa a corregir
+ * 
+ * public class holaMundo {
+    public static void main(String[] args) {
+        system.out.println("Hola, [nombre]!")
+        System.out.println("Bienvenidos al curso de programacion en Java);
+        Systemout.println(199x);
+        System.out.printl("Vamos a aprender mucho!")
+    }
+}
+ */
+
+
+package org.tpractica01;
+
+//Clase que inicia el progama
+public class HolaMundoAvanzado {
+	
+	// Método que lanza el programa
+	public static void main(String[] args) {
+		
+		// Corrección de la mayúscula de System y añadir nombre para imprimir 'Hola y nombre de usuario' 
+		System.out.println("Hola, [mvaltierra]!");
+		
+		// Corrección con el cierre de comillas del texto para imprimir 'Bienvenidos al curso de programación en Java'
+		System.out.println("Bienvenidos/as al curso de programación en Java");
+		
+		// Corrección del punto antes de out y añadir las comillas en el texto del año 
+		System.out.println("1995");
+		
+		// Corrección de println y añadir ';' al final de la sentencia
+		System.out.println("Vamos a aprender mucho!");
+		
+	}
+
+}		
+		
+		
+		
+		
+		
