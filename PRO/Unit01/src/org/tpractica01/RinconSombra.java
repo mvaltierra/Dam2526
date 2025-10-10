5b08d2a7546096fef44a6659be8aa57c9b3eb5cb
mvaltierra <mvaltierra@birt.eus>
2025-10-10 19:46:09 +0200

    correción errores programa tpractica01

--- PRO/Unit01/src/org/tpractica01/RinconSombra.java
+++ PRO/Unit01/src/org/tpractica01/RinconSombra.java
@@ -18,41 +18,41 @@ public class RinconSombra {
 	public static void main(String[] args) {
 	      
         System.out.println("Se encontraba el sauce en su rincón,");
-        System.out.println("Vino el roble y le hizo sombra,";
-        System.out,println("Sombreo el roble al sauce,");
-        System.out.println("El sauce a la sombra se quedo.";
+        System.out.println("Vino el roble y le hizo sombra,");
+        System.out.println("Sombreo el roble al sauce,");
+        System.out.println("El sauce a la sombra se quedo.");
         System.out.println();
 
         System.out.println("Se encontraba el roble en su rincon,");
         System.out.println("Vino el abedul y le hizo sombra,");
-        System.out.println(Sombreo el abedul al roble,");
-        System.out.pintln"Sombreo el roble al sauce,");
+        System.out.println("Sombreo el abedul al roble,");
+        System.out.println("Sombreo el roble al sauce,");
         System.out.println("El sauce a la sombra se quedo");
-        System.out.println
+        System.out.println();
 
         System.out.println("Se encontraba el abedul en su rincón,");
         System.out.println("Vino el pino y le hizo sombra,");
-        System.out.println("Sombreo el pino al abedul,"),
-        System.out.printn("Sombreo el abedul al rio,");
-        System.out.println("Sombreo el roble al sauce,").
+        System.out.println("Sombreo el pino al abedul,");
+        System.out.println("Sombreo el abedul al rio,");
+        System.out.println("Sombreo el roble al sauce,");
         System.out.println("El sauce a la sombra se quedo.");
         System.out.println();
 
         System.out.println("Se encontraba el pino en su rincón,");
-        System.out.println "Vino el cedro y le hizo sombra,");
+        System.out.println("Vino el cedro y le hizo sombra,");
         System.out.println("Sombreo el cedro al pino,");
         System.out.println("Sombreo el pino al abedul,");
-        System.out.println("Sombreo el abedul al rio,);
+        System.out.println("Sombreo el abedul al rio,");
         System.out.println("Sombreo el roble al sauce,");
         System.out.println("El sauce a la sombra se quedo.");
-        System.out.println()
+        System.out.println();
 
         System.out.println("Se encontraba el cedro en su rincón,");
         System.out.println("Vino el haya y le hizo sombra,");
         System.out.println("Sombreo el haya al cedro,");
         System.out.println("Sombreo el cedro al pino,");
         System.out.println("Sombreo el pino al abedul,");
-        system.out.println("Sombreo el abedul al roble,");
+        System.out.println("Sombreo el abedul al roble,");
         System.out.println("Sombreo el roble al sauce,");
         System.out.println("El sauce a la sombra se quedo.");
         System.out.println();
