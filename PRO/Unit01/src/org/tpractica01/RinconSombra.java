aff49feeeb4eedc000be82627a85a45aaa9749e4
mvaltierra <mvaltierra@birt.eus>
2025-10-10 21:09:42 +0200

    ultimas modificaciones al texto para el programa tpractica01

--- PRO/Unit01/src/org/tpractica01/RinconSombra.java
+++ PRO/Unit01/src/org/tpractica01/RinconSombra.java
@@ -55,7 +55,7 @@ public class RinconSombra {
 	
 	// Método para imprimir la segunda estrofa
 	public static void segundaEstrofa() {
-		System.out.println("Se encontraba el roble en su rincon,");
+		System.out.println("Se encontraba el roble en su rincón,");
         System.out.println("Vino el abedul y le hizo sombra,");
         System.out.println("Sombreo el abedul al roble,");
         
@@ -67,9 +67,12 @@ public class RinconSombra {
         System.out.println("Se encontraba el abedul en su rincón,");
         System.out.println("Vino el pino y le hizo sombra,");
         System.out.println("Sombreo el pino al abedul,");
-        System.out.println("Sombreo el abedul al rio,");
+        System.out.println("Sombreo el abedul al roble,");
         
         finPrimeraEstrofa();
+        
+       
+
         
 	}
 	
@@ -80,7 +83,7 @@ public class RinconSombra {
         System.out.println("Vino el cedro y le hizo sombra,");
         System.out.println("Sombreo el cedro al pino,");
         System.out.println("Sombreo el pino al abedul,");
-        System.out.println("Sombreo el abedul al rio,");
+        System.out.println("Sombreo el abedul al roble,");
      
         finPrimeraEstrofa();
 	}
